3543307db9856f4497180d600b4fe996a27c96b1
Max Lapan <max.lapan@gmail.com>
2019-07-25 14:51:37 +0300

    Rearrange chapters

--- Chapter12/lib/common.py
+++ Chapter12/lib/common.py
@@ -1,14 +1,51 @@
 import sys
 import time
-
 import numpy as np
-import torch
 
-import ptan
+import torch
+import torch.nn as nn
+
+
+class AtariA2C(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(AtariA2C, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.policy = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+        self.value = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.policy(conv_out), self.value(conv_out)
+
 
 class RewardTracker:
-    def __init__(self, writer):
+    def __init__(self, writer, stop_reward):
         self.writer = writer
+        self.stop_reward = stop_reward
 
     def __enter__(self):
         self.ts = time.time()
@@ -35,10 +72,13 @@ class RewardTracker:
         self.writer.add_scalar("speed", speed, frame)
         self.writer.add_scalar("reward_100", mean_reward, frame)
         self.writer.add_scalar("reward", reward, frame)
-        return mean_reward if len(self.total_rewards) > 30 else None
+        if mean_reward > self.stop_reward:
+            print("Solved in %d frames!" % frame)
+            return True
+        return False
 
 
-def unpack_batch(batch, net, last_val_gamma, device="cpu", states_preprocessor=ptan.agent.default_states_preprocessor):
+def unpack_batch(batch, net, last_val_gamma, device='cpu'):
     """
     Convert batch into training tensors
     :param batch:
@@ -51,23 +91,19 @@ def unpack_batch(batch, net, last_val_gamma, device="cpu", states_preprocessor=p
     not_done_idx = []
     last_states = []
     for idx, exp in enumerate(batch):
-        states.append(exp.state)
+        states.append(np.array(exp.state, copy=False))
         actions.append(int(exp.action))
         rewards.append(exp.reward)
         if exp.last_state is not None:
             not_done_idx.append(idx)
-            last_states.append(exp.last_state)
-    states_v = states_preprocessor(states)
-    if torch.is_tensor(states_v):
-        states_v = states_v.to(device)
+            last_states.append(np.array(exp.last_state, copy=False))
+    states_v = torch.FloatTensor(states).to(device)
     actions_t = torch.LongTensor(actions).to(device)
 
     # handle rewards
     rewards_np = np.array(rewards, dtype=np.float32)
     if not_done_idx:
-        last_states_v = states_preprocessor(last_states)
-        if torch.is_tensor(last_states_v):
-            last_states_v = last_states_v.to(device)
+        last_states_v = torch.FloatTensor(last_states).to(device)
         last_vals_v = net(last_states_v)[1]
         last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
         rewards_np[not_done_idx] += last_val_gamma * last_vals_np
---
+++ Chapter18/lib/common.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch
+
+import ptan
+
+
+def unpack_batch_a2c(batch, net, last_val_gamma, device="cpu"):
+    """
+    Convert batch into training tensors
+    :param batch:
+    :param net:
+    :return: states variable, actions tensor, reference values variable
+    """
+    states = []
+    actions = []
+    rewards = []
+    not_done_idx = []
+    last_states = []
+    for idx, exp in enumerate(batch):
+        states.append(exp.state)
+        actions.append(exp.action)
+        rewards.append(exp.reward)
+        if exp.last_state is not None:
+            not_done_idx.append(idx)
+            last_states.append(exp.last_state)
+    states_v = ptan.agent.float32_preprocessor(states).to(device)
+    actions_v = torch.FloatTensor(actions).to(device)
+
+    # handle rewards
+    rewards_np = np.array(rewards, dtype=np.float32)
+    if not_done_idx:
+        last_states_v = ptan.agent.float32_preprocessor(last_states).to(device)
+        last_vals_v = net(last_states_v)
+        last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
+        rewards_np[not_done_idx] += last_val_gamma * last_vals_np
+
+    ref_vals_v = torch.FloatTensor(rewards_np).to(device)
+    return states_v, actions_v, ref_vals_v
