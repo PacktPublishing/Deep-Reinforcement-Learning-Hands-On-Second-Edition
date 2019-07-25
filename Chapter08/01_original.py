6def4fc24796c41d19a9e40bd871c62ada88cfdb
Max Lapan <max.lapan@gmail.com>
2019-07-25 20:46:59 +0300

    Reimplement using ignite, tools will be ported to ptan

--- Chapter08/01_original.py
+++ Chapter08/01_original.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 import gym
 import ptan
-from datetime import datetime
+from datetime import datetime, timedelta
 import argparse
-import numpy as np
 
 import torch
 import torch.optim as optim
 
-from ignite.engine import Engine, Events
+from ignite.engine import Engine
 from ignite.metrics import RunningAverage
 from ignite.contrib.handlers import tensorboard_logger as tb_logger
 
-from lib import dqn_model, common
+from lib import dqn_model, common, ignite
 
 
 def batch_generator(buffer: ptan.experience.ExperienceReplayBuffer,
@@ -55,47 +54,39 @@ if __name__ == "__main__":
         epsilon_tracker.frame(engine.state.iteration)
         if engine.state.iteration % params.target_net_sync == 0:
             tgt_net.sync()
-        result = {
+        return {
             "loss": loss_v.item(),
             "epsilon": selector.epsilon,
         }
-        rewards = exp_source.pop_total_rewards()
-        if rewards:
-            print("%d: episode done, reward=%.2f" % (engine.state.iteration, rewards[0]))
-            result['reward'] = np.mean(rewards)
-        return result
 
     engine = Engine(process_batch)
-    logdir = f"runs/{datetime.now().isoformat()}-{params.run_name}-01_original"
+    ignite.EndOfEpisodeHandler(exp_source, bound_avg_reward=17.0).attach(engine)
+    ignite.EpisodeFPSHandler().attach(engine)
+
+    @engine.on(ignite.EndOfEpisodeHandler.Events.EPISODE_COMPLETED)
+    def episode_completed(trainer: Engine):
+        print("Episode %d: reward=%s, steps=%s, speed=%.3f frames/s" % (
+            trainer.state.episode, trainer.state.episode_reward,
+            trainer.state.episode_steps, trainer.state.metrics.get('fps', 0)))
+
+    @engine.on(ignite.EndOfEpisodeHandler.Events.BOUND_REWARD_REACHED)
+    def game_solved(trainer: Engine):
+        print("Game solved in %s, after %d episodes and %d iterations!" % (
+            timedelta(seconds=trainer.state.metrics['time_passed']),
+            trainer.state.episode, trainer.state.iteration))
+        trainer.should_terminate = True
+
+    logdir = f"runs/{datetime.now().isoformat(timespec='minutes')}-{params.run_name}-01_original"
     tb = tb_logger.TensorboardLogger(log_dir=logdir)
     RunningAverage(output_transform=lambda v: v['loss']).attach(engine, "avg_loss")
-    RunningAverage(output_transform=lambda v: v.get('reward', 0)).attach(engine, "avg_reward")
-    handler = tb_logger.OutputHandler(tag="train", metric_names=['avg_loss', 'avg_reward'],
+
+    episode_handler = tb_logger.OutputHandler(tag="episodes", metric_names=['reward', 'steps', 'avg_reward'])
+    tb.attach(engine, log_handler=episode_handler, event_name=ignite.EndOfEpisodeHandler.Events.EPISODE_COMPLETED)
+
+    # write to tensorboard every 100 iterations
+    ignite.PeriodicEvents().attach(engine)
+    handler = tb_logger.OutputHandler(tag="train", metric_names=['avg_loss', 'fps'],
                                       output_transform=lambda a: a)
-    tb.attach(engine, log_handler=handler, event_name=Events.ITERATION_COMPLETED)
+    tb.attach(engine, log_handler=handler, event_name=ignite.PeriodicEvents.Events.ITERATIONS_100_COMPLETED)
+
     engine.run(batch_generator(buffer, params.replay_initial, params.batch_size))
-    #
-    # frame_idx = 0
-    #
-    # with common.RewardTracker(writer, params.stop_reward) as reward_tracker:
-    #     while True:
-    #         frame_idx += 1
-    #         buffer.populate(1)
-    #         epsilon_tracker.frame(frame_idx)
-    #
-    #         new_rewards = exp_source.pop_total_rewards()
-    #         if new_rewards:
-    #             if reward_tracker.reward(new_rewards[0], frame_idx, selector.epsilon):
-    #                 break
-    #
-    #         if len(buffer) < params['replay_initial']:
-    #             continue
-    #
-    #         optimizer.zero_grad()
-    #         batch = buffer.sample(params['batch_size'])
-    #         loss_v = common.calc_loss_dqn(batch, net, tgt_net.target_model, gamma=params['gamma'], cuda=args.cuda)
-    #         loss_v.backward()
-    #         optimizer.step()
-    #
-    #         if frame_idx % params['target_net_sync'] == 0:
-    #             tgt_net.sync()
