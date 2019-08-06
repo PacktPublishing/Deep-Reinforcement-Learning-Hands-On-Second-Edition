cec6e873a1b5257dd67e0332dc37fb2e2d2e5c02
Max Lapan <max.lapan@gmail.com>
2019-08-06 09:35:42 +0300

    Rainbow without C51

--- Chapter08/08_dqn_rainbow_ign.py
+++ Chapter08/08_dqn_rainbow_ign.py
@@ -9,14 +9,17 @@ import torch.optim as optim
 
 from ignite.engine import Engine
 
-from lib import dqn_model, common, dqn_extra
+from lib import common, dqn_extra
 
-NAME = "05_prio_replay"
+NAME = "08_rainbow"
+N_STEPS = 4
 PRIO_REPLAY_ALPHA = 0.6
 
 
-def calc_loss(batch, batch_weights, net, tgt_net, gamma, device="cpu"):
-    states, actions, rewards, dones, next_states = common.unpack_batch(batch)
+def calc_loss_rainbow(batch, batch_weights, net, tgt_net, gamma,
+                      device="cpu", double=True):
+    states, actions, rewards, dones, next_states = \
+        common.unpack_batch(batch)
 
     states_v = torch.tensor(states).to(device)
     actions_v = torch.tensor(actions).to(device)
@@ -24,13 +27,24 @@ def calc_loss(batch, batch_weights, net, tgt_net, gamma, device="cpu"):
     done_mask = torch.ByteTensor(dones).to(device)
     batch_weights_v = torch.tensor(batch_weights).to(device)
 
-    state_action_values = net(states_v).gather(1, actions_v.unsqueeze(-1)).squeeze(-1)
+    actions_v = actions_v.unsqueeze(-1)
+    state_action_values = net(states_v).gather(1, actions_v)
+    state_action_values = state_action_values.squeeze(-1)
     with torch.no_grad():
         next_states_v = torch.tensor(next_states).to(device)
-        next_state_values = tgt_net(next_states_v).max(1)[0]
+        if double:
+            next_state_actions = net(next_states_v).max(1)[1]
+            next_state_actions = next_state_actions.unsqueeze(-1)
+            next_state_values = tgt_net(next_states_v).gather(
+                1, next_state_actions).squeeze(-1)
+        else:
+            next_state_values = tgt_net(next_states_v).max(1)[0]
         next_state_values[done_mask] = 0.0
-        expected_state_action_values = next_state_values.detach() * gamma + rewards_v
-    losses_v = batch_weights_v * (state_action_values - expected_state_action_values) ** 2
+        expected_state_action_values = \
+            next_state_values.detach() * gamma + rewards_v
+    losses_v = (state_action_values -
+                expected_state_action_values) ** 2
+    losses_v *= batch_weights_v
     return losses_v.mean(), (losses_v + 1e-5).data.cpu().numpy()
 
 
@@ -39,7 +53,8 @@ if __name__ == "__main__":
     torch.manual_seed(common.SEED)
     params = common.HYPERPARAMS['pong']
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cuda", default=False, action="store_true", help="Enable cuda")
+    parser.add_argument("--cuda", default=False,
+                        action="store_true", help="Enable cuda")
     args = parser.parse_args()
     device = torch.device("cuda" if args.cuda else "cpu")
 
@@ -47,37 +62,37 @@ if __name__ == "__main__":
     env = ptan.common.wrappers.wrap_dqn(env)
     env.seed(common.SEED)
 
-    net = dqn_model.DQN(env.observation_space.shape, env.action_space.n).to(device)
+    net = dqn_extra.RainbowDQN(env.observation_space.shape,
+                        env.action_space.n).to(device)
 
     tgt_net = ptan.agent.TargetNet(net)
-    selector = ptan.actions.EpsilonGreedyActionSelector(epsilon=params.epsilon_start)
-    epsilon_tracker = common.EpsilonTracker(selector, params)
+    selector = ptan.actions.ArgmaxActionSelector()
     agent = ptan.agent.DQNAgent(net, selector, device=device)
 
     exp_source = ptan.experience.ExperienceSourceFirstLast(
-        env, agent, gamma=params.gamma)
+        env, agent, gamma=params.gamma, steps_count=N_STEPS)
     buffer = dqn_extra.PrioReplayBuffer(
         exp_source, params.replay_size, PRIO_REPLAY_ALPHA)
-    optimizer = optim.Adam(net.parameters(), lr=params.learning_rate)
+    optimizer = optim.Adam(net.parameters(),
+                           lr=params.learning_rate)
 
     def process_batch(engine, batch_data):
         batch, batch_indices, batch_weights = batch_data
         optimizer.zero_grad()
-        loss_v, sample_prios = calc_loss(
+        loss_v, sample_prios = calc_loss_rainbow(
             batch, batch_weights, net, tgt_net.target_model,
-            gamma=params.gamma, device=device)
+            gamma=params.gamma**N_STEPS, device=device)
         loss_v.backward()
         optimizer.step()
         buffer.update_priorities(batch_indices, sample_prios)
-        epsilon_tracker.frame(engine.state.iteration)
         if engine.state.iteration % params.target_net_sync == 0:
             tgt_net.sync()
         return {
             "loss": loss_v.item(),
-            "epsilon": selector.epsilon,
             "beta": buffer.update_beta(engine.state.iteration),
         }
 
     engine = Engine(process_batch)
     common.setup_ignite(engine, params, exp_source, NAME)
-    engine.run(common.batch_generator(buffer, params.replay_initial, params.batch_size))
+    engine.run(common.batch_generator(buffer, params.replay_initial,
+                                      params.batch_size))
--- Chapter08/lib/dqn_extra.py
+++ Chapter08/lib/dqn_extra.py
@@ -281,3 +281,42 @@ def distr_projection(next_distr, rewards, dones, gamma):
             proj_distr[ne_dones, l[ne_mask]] = (u - b_j)[ne_mask]
             proj_distr[ne_dones, u[ne_mask]] = (b_j - l)[ne_mask]
     return proj_distr
+
+
+class RainbowDQN(nn.Module):
+    def __init__(self, input_shape, n_actions):
+        super(RainbowDQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc_adv = nn.Sequential(
+            NoisyLinear(conv_out_size, 256),
+            nn.ReLU(),
+            NoisyLinear(256, n_actions)
+        )
+        self.fc_val = nn.Sequential(
+            nn.Linear(conv_out_size, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
+
+    def forward(self, x):
+        adv, val = self.adv_val(x)
+        return val + (adv - adv.mean(dim=1, keepdim=True))
+
+    def adv_val(self, x):
+        fx = x.float() / 256
+        conv_out = self.conv(fx).view(fx.size()[0], -1)
+        return self.fc_adv(conv_out), self.fc_val(conv_out)
