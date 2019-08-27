7802490378c3f624feb061f30bf5a2c41053bbc2
Max Lapan <max.lapan@gmail.com>
2019-08-27 09:27:12 +0300

    Tweak scripts

--- Chapter15/games/make-simple-lots.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#tw-make custom --world-size 5 --nb-objects 10 --quest-length 5  --quest-breadth 1 --seed 0 --output simple-val.ulx
-
-for i in `seq 101 500`; do
-    tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed $i --output simple$i.ulx
-done
--- Chapter15/games/make_games.sh
+++ Chapter15/games/make_games.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed 1234 --output simple1.ulx
-tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed 1235 --output simple2.ulx
-tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed 1236 --output simple3.ulx
-tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed 1237 --output simple4.ulx
-tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed 1238 --output simple5.ulx
-tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed 1239 --output simple-val.ulx
+tw-make custom --world-size 5 --nb-objects 10 --quest-length 5  --quest-breadth 1 --seed 0 --output simple-val.ulx
 
-tw-make custom --world-size 10 --nb-objects 20 --quest-length 7 --quest-breadth 2 --seed 1234 --output medium1.ulx
-tw-make custom --world-size 10 --nb-objects 20 --quest-length 7 --quest-breadth 2 --seed 1235 --output medium2.ulx
-tw-make custom --world-size 10 --nb-objects 20 --quest-length 7 --quest-breadth 2 --seed 1236 --output medium3.ulx
-tw-make custom --world-size 10 --nb-objects 20 --quest-length 7 --quest-breadth 2 --seed 1237 --output medium4.ulx
-tw-make custom --world-size 10 --nb-objects 20 --quest-length 7 --quest-breadth 2 --seed 1238 --output medium5.ulx
-tw-make custom --world-size 10 --nb-objects 20 --quest-length 7 --quest-breadth 2 --seed 1239 --output medium-val.ulx
+# change the range to generate more games
+for i in `seq 1 20`; do
+    tw-make custom --world-size 5 --nb-objects 10 --quest-length 5 --quest-breadth 1 --seed $i --output simple$i.ulx
+done
