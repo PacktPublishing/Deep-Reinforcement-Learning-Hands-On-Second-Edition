Bug in ppo ref vals calculation

Chapter21/lib/ppo.py
@@ -1,4 +1,5 @@
 import ptan
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -61,6 +62,8 @@ def calc_adv_ref(values, dones, rewards, gamma, gae_lambda):
             last_gae = delta + gamma * gae_lambda * last_gae
         adv.append(last_gae)
         ref.append(last_gae + val)
+    adv = list(reversed(adv))
+    ref = list(reversed(ref))
     return torch.FloatTensor(adv), torch.FloatTensor(ref)
 
 
@@ -73,11 +76,14 @@ def batch_generator(exp_source: ptan.experience.ExperienceSource,
     trj_actions = []
     trj_rewards = []
     trj_dones = []
+    final_states = []
     for (exp,) in exp_source:
         trj_states.append(exp.state)
         trj_actions.append(exp.action)
         trj_rewards.append(exp.reward)
         trj_dones.append(exp.done)
+        if exp.done:
+            final_states.append(np.array(exp.state, copy=True))
         if len(trj_states) < trajectory_size:
             continue
         trj_states_t = torch.FloatTensor(trj_states).to(device)
@@ -98,16 +104,18 @@ def batch_generator(exp_source: ptan.experience.ExperienceSource,
 
         # generate needed amount of batches
         for _ in range(ppo_epoches):
-            for batch_ofs in range(0, len(trj_states)-1, batch_size):
+            for batch_ofs in range(1, len(trj_states)-batch_size, batch_size):
                 yield (
                     trj_states_t[batch_ofs:batch_ofs + batch_size],
                     trj_actions_t[batch_ofs:batch_ofs + batch_size],
                     adv_t[batch_ofs:batch_ofs + batch_size],
                     ref_t[batch_ofs:batch_ofs + batch_size],
-                    old_logprob_t[batch_ofs:batch_ofs + batch_size]
+                    old_logprob_t[batch_ofs:batch_ofs + batch_size],
+                    final_states,
                 )
 
         trj_states.clear()
         trj_actions.clear()
         trj_rewards.clear()
         trj_dones.clear()
+        final_states.clear()

Chapter21/mcar_ppo.py
@@ -4,6 +4,7 @@ import gym
 import argparse
 import random
 import torch
+import numpy as np
 import torch.optim as optim
 import torch.nn.functional as F
 
@@ -17,14 +18,15 @@ HYPERPARAMS = {
         'env_name':         "CartPole-v0",
         'stop_reward':      190.0,
         'run_name':         'debug',
-        'learning_rate':    1e-4,
-        'gamma':            0.99,
+        'actor_lr':         1e-4,
+        'critic_lr':        1e-4,
+        'gamma':            0.9,
         'ppo_trajectory':   2049,
         'ppo_epoches':      10,
         'ppo_eps':          0.2,
         'batch_size':       32,
         'gae_lambda':       0.95,
-        'entropy_beta':     0.0,
+        'entropy_beta':     0.1,
     }),
     'ppo': SimpleNamespace(**{
         'env_name':         "MountainCar-v0",
@@ -96,15 +98,23 @@ if __name__ == "__main__":
 
     agent = ptan.agent.PolicyAgent(net.actor, apply_softmax=True, preprocessor=ptan.agent.float32_preprocessor)
     exp_source = ptan.experience.ExperienceSource(env, agent, steps_count=1)
-    optimizer = optim.Adam(net.parameters(), lr=params.learning_rate)
+    opt_actor = optim.Adam(net.actor.parameters(), lr=params.actor_lr)
+    opt_critic = optim.Adam(net.critic.parameters(), lr=params.critic_lr)
 
     def process_batch(engine, batch):
-        optimizer.zero_grad()
+        states_t, actions_t, adv_t, ref_t, old_logprob_t, final_states = batch
 
-        states_t, actions_t, adv_t, ref_t, old_logprob_t = batch
-        policy_t, value_t = net(states_t)
+        final_vals_t = net.critic(torch.FloatTensor(final_states))
+        final_vals = torch.mean(final_vals_t).item()
+
+        opt_critic.zero_grad()
+        value_t = net.critic(states_t)
         loss_value_t = F.mse_loss(value_t.squeeze(-1), ref_t)
+        loss_value_t.backward()
+        opt_critic.step()
 
+        opt_actor.zero_grad()
+        policy_t = net.actor(states_t)
         logpolicy_t = F.log_softmax(policy_t, dim=1)
 
         prob_t = F.softmax(policy_t, dim=1)
@@ -115,18 +125,22 @@ if __name__ == "__main__":
         surr_obj_t = adv_t * ratio_t
         clipped_surr_t = adv_t * torch.clamp(ratio_t, 1.0 - params.ppo_eps, 1.0 + params.ppo_eps)
         loss_policy_t = -torch.min(surr_obj_t, clipped_surr_t).mean()
+        loss_polent_t = params.entropy_beta * loss_entropy_t + loss_policy_t
+        loss_polent_t.backward()
+        opt_actor.step()
 
-        loss_t = loss_value_t + params.entropy_beta * loss_entropy_t + loss_policy_t
-        loss_t.backward()
-        optimizer.step()
-        return {
-            "loss": loss_t.item(),
+        res = {
+            "loss": loss_value_t.item() + loss_polent_t.item(),
             "loss_value": loss_value_t.item(),
             "loss_policy": loss_policy_t.item(),
             "adv": adv_t.mean().item(),
             "ref": ref_t.mean().item(),
             "loss_entropy": loss_entropy_t.item(),
+            "final_vals": final_vals,
         }
+        return res
+
+
 
     engine = Engine(process_batch)
     common.setup_ignite(engine, params, exp_source, args.name)