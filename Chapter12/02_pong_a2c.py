912010bdfe942dcb75d56ce8f4ff2cd1fd2432f5
Max Lapan <max.lapan@gmail.com>
2019-11-06 20:33:43 +0300

    Severe torch perfromance regression

--- Chapter12/02_pong_a2c.py
+++ Chapter12/02_pong_a2c.py
@@ -72,23 +72,26 @@ def unpack_batch(batch, net, device='cpu'):
     not_done_idx = []
     last_states = []
     for idx, exp in enumerate(batch):
-        states.append(np.array(exp.state, copy=False))
+        states.append(torch.FloatTensor(np.array(exp.state, copy=False)))
         actions.append(int(exp.action))
         rewards.append(exp.reward)
         if exp.last_state is not None:
             not_done_idx.append(idx)
-            last_states.append(np.array(exp.last_state, copy=False))
-    states_v = torch.FloatTensor(states).to(device)
+            last_states.append(torch.FloatTensor(np.array(exp.last_state, copy=False)))
+
+    states_v = torch.stack(states).to(device)
     actions_t = torch.LongTensor(actions).to(device)
+
     # handle rewards
     rewards_np = np.array(rewards, dtype=np.float32)
     if not_done_idx:
-        last_states_v = torch.FloatTensor(last_states).to(device)
+        last_states_v = torch.stack(last_states).to(device)
         last_vals_v = net(last_states_v)[1]
         last_vals_np = last_vals_v.data.cpu().numpy()[:, 0]
         rewards_np[not_done_idx] += GAMMA ** REWARD_STEPS * last_vals_np
 
     ref_vals_v = torch.FloatTensor(rewards_np).to(device)
+
     return states_v, actions_t, ref_vals_v
 
 
