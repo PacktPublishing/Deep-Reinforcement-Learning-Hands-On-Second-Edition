0a7c05f261ddc44b1acfc2bdf763cfedbe267c83
therealmarvin <nakedtoothbrush@hotmail.com>
2020-10-31 15:20:01 -0400

    I figured they wanted to use the agent environment and not the global variable

--- Chapter06/02_dqn_pong.py
+++ Chapter06/02_dqn_pong.py
@@ -62,7 +62,7 @@ class Agent:
         self._reset()
 
     def _reset(self):
-        self.state = env.reset()
+        self.state = self.env.reset()
         self.total_reward = 0.0
 
     @torch.no_grad()
@@ -70,7 +70,7 @@ class Agent:
         done_reward = None
 
         if np.random.random() < epsilon:
-            action = env.action_space.sample()
+            action = self.env.action_space.sample()
         else:
             state_a = np.array([self.state], copy=False)
             state_v = torch.tensor(state_a).to(device)
