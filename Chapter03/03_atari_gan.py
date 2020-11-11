Fixed normalization
Bug in the normalization - the "-1" was applied to the factor instead of the value.

Chapter03/03_atari_gan.py
@@ -127,7 +127,8 @@ def iterate_batches(envs, batch_size=BATCH_SIZE):
         if len(batch) == batch_size:
             # Normalising input between -1 to 1
             batch_np = np.array(batch, dtype=np.float32)
-            batch_np *= 2.0 / 255.0 - 1.0
+            batch_np *= 2. / 255.
+            batch_np -= 1.
             yield torch.tensor(batch_np)
             batch.clear()
         if is_done: