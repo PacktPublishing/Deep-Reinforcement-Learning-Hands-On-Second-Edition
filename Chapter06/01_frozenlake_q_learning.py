f607c5bd425ab4068d25a51e60396bddc82ad6a7
Marion <marion.baumgartner@camptocamp.com>
2023-01-22 11:24:42 +0100

    upgrade to FL v1 and adapt code

--- Chapter06/01_frozenlake_q_learning.py
+++ Chapter06/01_frozenlake_q_learning.py
@@ -3,7 +3,7 @@ import gym
 import collections
 from tensorboardX import SummaryWriter
 
-ENV_NAME = "FrozenLake-v0"
+ENV_NAME = "FrozenLake-v1"
 GAMMA = 0.9
 ALPHA = 0.2
 TEST_EPISODES = 20
@@ -12,14 +12,14 @@ TEST_EPISODES = 20
 class Agent:
     def __init__(self):
         self.env = gym.make(ENV_NAME)
-        self.state = self.env.reset()
+        self.state, _ = self.env.reset()
         self.values = collections.defaultdict(float)
 
     def sample_env(self):
         action = self.env.action_space.sample()
         old_state = self.state
-        new_state, reward, is_done, _ = self.env.step(action)
-        self.state = self.env.reset() if is_done else new_state
+        new_state, reward, is_done, terminated, _ = self.env.step(action)
+        self.state, _ = self.env.reset() if is_done or terminated else (new_state, None)
         return old_state, action, reward, new_state
 
     def best_value_and_action(self, state):
@@ -39,12 +39,12 @@ class Agent:
 
     def play_episode(self, env):
         total_reward = 0.0
-        state = env.reset()
+        state, _ = env.reset()
         while True:
             _, action = self.best_value_and_action(state)
-            new_state, reward, is_done, _ = env.step(action)
+            new_state, reward, is_done, terminated, _ = env.step(action)
             total_reward += reward
-            if is_done:
+            if is_done or terminated:
                 break
             state = new_state
         return total_reward
