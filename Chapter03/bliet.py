5baaf3f86ef1d802cd4b9cde6ee0e69318c27afd
Narachara <pwinterle@gmx.de>
2024-02-13 22:55:25 +0100

    fixed 01_cartpole in Chapter04

--- Chapter03/bliet.py
+++ Chapter03/bliet.py
@@ -1,7 +1,12 @@
-import gymnasium as gym
-
+import gymnasium  as gym
 
 if __name__ == "__main__":
     env = gym.make("CartPole-v1")
+
+    total_reward = 0.0
+    total_steps = 0
     obs = env.reset()
-    print(env.observation_space.shape)
+    obs = obs[0]
+
+    print(obs)
+
